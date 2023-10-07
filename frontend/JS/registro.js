5db2c43c85a9a987e2441846f2c03c6e0cb56bca
lauradorock <laurapadilhamarques@gmail.com>
2023-10-07 11:09:29 -0300

    iniciando projeto

---
+++ frontend/JS/registro.js
@@ -0,0 +1,18 @@
+async function registro() {
+    const nome = document.getElementById('nome').val();
+
+    const data = {
+        nome
+    }
+
+    const response = fetch('http://localhost:3008/api/user/create', data);
+
+    if (response.data.success === true) {
+        // redirioba para logar
+    } else {
+        alert("Nao deu pra logar")
+    }
+    
+
+
+}
